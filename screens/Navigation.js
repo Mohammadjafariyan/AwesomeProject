c2c678abd06949161fbdfafd1065ac08ee366997
moham <moham@DESKTOP-AS136SM>
2019-08-30 21:04:25 +0430

    1.0

--- screens/Navigation.js
+++ screens/Navigation.js
@@ -1,25 +1,34 @@
-/*
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
-import HomeScreen from "./home/HomeScreen";
 import AnimateHome from "./home/AnimateHome";
 import React from "react";
 import {View} from "react-native-animatable";
 
 class AppStartScreen extends React.Component {
+    static navigationOptions = {
+        header: null
+    }
+    rendermine(){
+
+    }
+
     render() {
         return (
-            <View   >
-             <AnimateHome/>
+            <View style={{ flex: 1,height:100  }}   >
+                <AnimateHome/>
             </View>
         );
     }
 }
 
-const AppNavigator = createStackNavigator({
-    Home: {
-        screen: AppStartScreen
+const AppNavigator = createStackNavigator(
+    {
+        Home: AppStartScreen,
+    },
+    {
+        initialRouteName: "Home"
     }
-});
+);
 
-export default createAppContainer(AppNavigator);*/
+
+export default createAppContainer(AppNavigator);
